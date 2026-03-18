3dbbe231458d64b54dbe17ca1060385e675f7870
Samuel y Kevin <sapineda55@ucatolica.edu.co>
2026-03-18 12:56:15 -0500

    Creacion de funcion que pida dos parametros

--- main.py
+++ main.py
@@ -13,3 +13,14 @@ def mensaje_n_c(nombre:str,codigo:int):
 @app.get('/uno')
 def mensaje_edad(edad:int):
     return f'tu edad es {edad}'
+
+@app.get('/dos')
+def mensaje_E (estatura:float,peso:float):
+    while estatura <= 1.70:
+        if peso >70.0:
+            return f'Tras de que mide {estatura} esta gordo  {peso} ?'
+    while estatura > 1.70: 
+        if peso <=65.0:
+            return f'Buena estatura bro {estatura} pero no estaria mal comer'
+        else:
+            return f'Buena estatura bro {estatura} pero cuida el peso pa'
