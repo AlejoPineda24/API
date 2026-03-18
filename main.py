7a8c84421d54663598233175a46e5e3213363320
Samuel y Kevin <sapineda55@ucatolica.edu.co>
2026-03-18 12:54:47 -0500

    Creacion de linea que pida edad de API

--- main.py
+++ main.py
@@ -9,3 +9,7 @@ def mensaje():
 @app.get('/{nombre}/{codigo}')
 def mensaje_n_c(nombre:str,codigo:int):
     return f'Bienvenido {nombre} su codigo es {codigo}'
+
+@app.get('/uno')
+def mensaje_edad(edad:int):
+    return f'tu edad es {edad}'
